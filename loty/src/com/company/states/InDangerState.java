9230bb28fd44e64905691bf6e82ab7c614da4b87
Dawid Bielecki <dbielecki97@gmail.com>
2019-12-18 11:37:47 +0100

    zmniejszenie liczby stanów wykorzystując dekoratory, zaimplementowano wzorzec DEKORATOR

--- loty/src/com/company/states/InDangerState.java
+++ loty/src/com/company/states/InDangerState.java
@@ -14,21 +14,11 @@ public class InDangerState extends State {
 
     @Override
     public void draw(Statek context, Graphics2D g) {
-        g.setColor(pulsingColor(new Color(255, 19, 0, 255), 0.6));
-        final float[] dash1 = {2.5f};
-        final BasicStroke dashed =
-                new BasicStroke(2.5f,
-                        BasicStroke.CAP_BUTT,
-                        BasicStroke.JOIN_ROUND,
-                        1.0f, dash1, 0.0f);
-        g.setStroke(dashed);
-        g.draw(new Area(new Ellipse2D.Double(context.getPozycja().x - 25, context.getPozycja().y - 25, 50, 50)));
-        g.setColor(context.getColor());
         g.setColor(context.getColor());
         context.drawShape(g);
     }
 
-    public void avoid(){
+    public void avoid() {
         //TODO napisać implementację metody avoid() oraz strategie
     }
 }
--- loty/src/com/company/states/NormalState.java
+++ loty/src/com/company/states/NormalState.java
@@ -9,7 +9,6 @@ public class NormalState extends State {
 
     @Override
     public void draw(Statek context, Graphics2D g) {
-        g.setColor(context.getColor());
         context.drawShape(g);
     }
 }
